4240af550c97f94f535a8a7dfaa00ae603945139
Jacky-Sanjaya <kykho3@gmail.com>
2021-03-18 15:27:13 +0700

    adding a few testing

--- Flickr/__tests__/App-test.js
+++ Flickr/__tests__/App-test.js
@@ -2,13 +2,26 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-import App from '../App';
+import {shallow} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import MainScreen from '../src/screen/MainScreen/MainScreen.js';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+Enzyme.configure({adapter: new Adapter()});
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('MainScreen', () => {
+  describe('Rendering', () => {
+    it('should match to snapshot', () => {
+      const component = shallow(<Mainscreen />);
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
+
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'enzyme',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+};
